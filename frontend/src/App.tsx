a9c884a2aa8791b56c2089faab70171dd44b8198
Raquel Ciusz <raquel.ciusz@yahoo.com>
2024-01-28 12:11:55 +0100

    login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,11 @@ import AppBar from './app/features/appbar/AppBar';
 import PersistLogin from './app/features/sessions/PersistLogin';
 import PrivateRoute from './app/features/routes/PrivateRoute';
 import Dashboard from './app/features/dashboard/Dashboard';
+import Logout from './app/features/sessions/Logout';
+import UpdateProfile from './app/features/sessions/UpdateProfile';
+import Login from './app/features/sessions/Login';
+import Signup from './app/features/sessions/Signup';
+import PublicOnlyRoute from './app/features/routes/PublicOnlyRoute';
 
 function App() {
   return (
@@ -19,6 +24,26 @@ function App() {
                 <PrivateRoute>
                   <Dashboard />
                 </PrivateRoute>
+              } />
+              <Route path="/logout" element={
+                <PrivateRoute>
+                  <Logout />
+                </PrivateRoute>
+              } />
+              <Route path="/update-profile" element={
+                <PrivateRoute>
+                  <UpdateProfile />
+                </PrivateRoute>
+              } />
+              <Route path="/login" element={
+                <PublicOnlyRoute>
+                  <Login />
+                </PublicOnlyRoute>
+              } />
+              <Route path="/signup" element={
+                <PublicOnlyRoute>
+                  <Signup />
+                </PublicOnlyRoute>
               } />
             </Route>
           </Routes>
